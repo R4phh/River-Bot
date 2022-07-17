43517965bbe7a2229479bc153d584aa9c73cbf48
R4phh <raphaelantoniorecio@gmail.com>
2022-07-17 10:42:04 +0800

    initialize github repo

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js")
+
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: ["GUILDS"]
+})
+
+let bot = {
+    client
+}
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.slashcommands = new Discord.Collection() 
+
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+client.loadSlashCommands(bot, false)
+
+client.on("interactionCreate", (interaction) => {
+    if (!interaction.isCommand()) return 
+    if (!interaction.inGuild()) return interaction.reply("This command can only be used in a server")
+
+    const slashcmd = client.slashcommands.get(interaction.commandName)
+
+    if (!slashcmd) return interaction.reply("Invalid slash command")
+
+    if (slashcmd.perm && !interaction.member.permissions.has(slashcmd.perm))
+        return interaction.reply("You do not have permission for this command")
+
+    slashcmd.run(client, interaction)
+})
+const welcomeChannelId = "989390125874155620"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `― Hey there <@${member.id}>! Welcome to the Underworld office. Before you help us, youre gonna need to check out the rules channel.`,
+        files: [img]
+    })
+})
+client.login(process.env.TOKEN)
