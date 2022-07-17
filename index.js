2bf174c102662dac6dc829822062792d6f101cda
R4phh <raphaelantoniorecio@gmail.com>
2022-07-17 15:28:18 +0800

    first commit

--- index.js
+++ index.js
@@ -41,4 +41,21 @@ client.on("guildMemberAdd", async (member) => {
         files: [img]
     })
 })
+
+const guildId = "978325045061509210"
+
+client.slashcommands = new Discord.Collection() 
+
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+client.loadSlashCommands(bot, false)
+
+client.on("ready", async () => {
+    const guild = client.guilds.cache.get(guildId)
+    if (!guild)
+        return console.error("Target guild not found")
+    
+    await guild.commands.set([...client.slashcommands.values()])
+    console.log(`Successfully loaded in ${client.slashcommands.size}`)
+})
+
 client.login(process.env.TOKEN)
